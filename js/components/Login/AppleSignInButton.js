4906a7838a7fce06c33d04b0806960e76aec5c53
Wanyoung Kim <80432333+astral-monad@users.noreply.github.com>
2022-10-19 21:35:33 +0100

    Update AppleSignInButton.js

--- js/components/Login/AppleSignInButton.js
+++ js/components/Login/AppleSignInButton.js
@@ -10,7 +10,7 @@ import theme from '../../../styles/theme';
 
 import {loginViaApple} from '../../redux/actions/user';
 
-import {goToWelcomeScreen, goToMainScreen} from 'js/navigation/goToActions';
+import {goToWelcomeScreen, goToMainScreen} from '../../navigation/goToActions';
 
 const AppleSignIn = props => {
     const onLoginFinished = token => {
