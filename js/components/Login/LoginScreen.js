0b3088efbeacdbd09ba7e08a8218ff8025546b46
Wanyoung Kim <80432333+astral-monad@users.noreply.github.com>
2022-10-19 19:36:29 +0100

    Update LoginScreen.js

--- js/components/Login/LoginScreen.js
+++ js/components/Login/LoginScreen.js
@@ -10,7 +10,7 @@ import {
     goToEmailScreen,
     goToWelcomeScreen,
     goToMainScreen,
-} from 'js/navigation/goToActions';
+} from 'js/navigation/gotoactions';
 import FBLoginButton from './FBLoginButton';
 import AppleSignInButton from './AppleSignInButton';
 import SwiperCard, {dot, activeDot} from './SwiperCard';
