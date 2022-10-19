cc42e5033b2a9fe52fc41a3f4c6f3ac4a9a32902
Wanyoung Kim <80432333+astral-monad@users.noreply.github.com>
2022-10-19 20:00:59 +0100

    Create theme.js

---
+++ styles/theme.js
@@ -0,0 +1,33 @@
+export default {
+    // Major Third Type Scale
+    FONT_SIZE_0: 12,
+    FONT_SIZE_1: 16,
+    FONT_SIZE_2: 20,
+    FONT_SIZE_3: 25,
+    FONT_SIZE_4: 31.25,
+    FONT_SIZE_5: 39.06,
+    FONT_SIZE_6: 48.83,
+
+    FONT_WEIGHT_1: '300',
+    FONT_WEIGHT_2: '600',
+    FONT_WEIGHT_3: '800',
+
+    // 'white', 'gray' and 'black' are also used
+    COLOR_1: '#181818', // Light Black
+    COLOR_2: '#E2241E', // Red
+    COLOR_3: '#212121', // Dark Gray
+    COLOR_4: '#969696', // Light Gray
+    COLOR_5: '#AAAAAA', // Dimmed White
+    COLOR_6: '#00000090', //Transparent Black
+    COLOR_7: '#333333', // Medium Gray
+    COLOR_8: '#101010', //Dark Black
+
+    BORDER_RADIUS_1: 5,
+    BORDER_RADIUS_2: 10,
+    BORDER_RADIUS_3: 50,
+
+    OPACITY_1: 0.3,
+    OPACITY_2: 0.5,
+    OPACITY_3: 0.7,
+};
+
