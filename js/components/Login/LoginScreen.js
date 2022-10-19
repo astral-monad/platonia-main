03b0f1e25ef34f3381f621d1b11b6234eec88efa
Wanyoung Kim <80432333+astral-monad@users.noreply.github.com>
2022-10-19 20:04:33 +0100

    Update LoginScreen.js

--- js/components/Login/LoginScreen.js
+++ js/components/Login/LoginScreen.js
@@ -17,7 +17,7 @@ import SwiperCard, {dot, activeDot} from './SwiperCard';
 import PlatoniaLogo from './PlatoniaLogo';
 import NavBtn from 'platonia-main/js/components/Global/NavigationBtn';
 import BigRedButton from 'platonia-main/js/components/Global/BigRedButton';
-import theme from 'platonia-main/styles/theme';
+import theme from 'platonia-main/styles/theme.js';
 
 const LoginScreen = props => {
     const onLoginFinished = (error, result) => {
