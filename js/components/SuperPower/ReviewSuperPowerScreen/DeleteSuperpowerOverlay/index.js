ab64bb2d2ca35e2f22108491b8b5eb97ee29e2b1
Wanyoung Kim <80432333+astral-monad@users.noreply.github.com>
2022-10-21 23:05:16 +0100

    Update index.js

--- js/components/SuperPower/ReviewSuperPowerScreen/DeleteSuperpowerOverlay/index.js
+++ js/components/SuperPower/ReviewSuperPowerScreen/DeleteSuperpowerOverlay/index.js
@@ -5,8 +5,8 @@ import {Navigation} from 'react-native-navigation';
 import PropTypes from 'prop-types';
 // Local Imports
 import ContentCard from './ContentCard';
-import OverlayButton from 'js/components/Global/OverlayButton';
-import theme from 'platonia_client/styles/theme';
+import OverlayButton from '../../../Global/OverlayButton';
+import theme from '../../../../../styles/theme';
 
 const DeleteSuperpowerOverlay = props => {
     const animationDuration = 500;
